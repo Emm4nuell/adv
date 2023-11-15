e20fb770f34e4b20f19516b6b5c4c802c49cc3a1
Emm4nuell <eduardo.eesf@gmail.om>
2023-11-15 11:44:29 -0300

    Criado component NAV

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Componentes do sistema
+import { NavComponent } from './components/nav/nav.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
